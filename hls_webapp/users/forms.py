1a72bccf01683caab8ac09c081e9771dbdf344d2
nacastroprice <ncastroprice@gmail.com>
2020-02-16 21:06:10 -0600

    Can now get select info from SelectType

--- hls_webapp/users/forms.py
+++ hls_webapp/users/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
 from flask_login import current_user
 from hls_webapp.models import User
 
@@ -19,12 +19,14 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError(
+                'That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -40,10 +42,10 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
 
     audio = FileField('Upload .wav file', validators=[FileAllowed(['wav'])])
-
 
     submit = SubmitField('Update')
 
@@ -51,13 +53,15 @@ class UpdateAccountForm(FlaskForm):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
+                raise ValidationError(
+                    'That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
+                raise ValidationError(
+                    'That email is taken. Please choose a different one.')
 
 
 class RequestResetForm(FlaskForm):
@@ -68,7 +72,8 @@ class RequestResetForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
+            raise ValidationError(
+                'There is no account with that email. You must register first.')
 
 
 class ResetPasswordForm(FlaskForm):
@@ -77,11 +82,16 @@ class ResetPasswordForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
 
+
 class SimulationOptions(FlaskForm):
 
-    group_id = SelectField(u'Sound Group', coerce=int)
+    group_id = SelectField(u'Sound Group', coerce=int,
+                           validators=[InputRequired()])
 
-    def edit_user(request, id):
-        # user = current_user.id:
-        form = SimulationOptions(request.POST, obj=current_user.sound_file)
-        form.group_id.choices = [(g.id, g.file_name) for g in Group.query.all()]
+    submit = SubmitField('Submit simulation preferences')
+
+    # def validate_sim_options(self, obj):
+    #     # user = current_user.id:
+    #     form = SimulationOptions(request.POST, obj=current_user.sound_file)
+    #     form.group_id.choices = [(g.id, g.file_name)
+    #                              for g in Group.query.all()]
--- hls_webapp/users/routes.py
+++ hls_webapp/users/routes.py
@@ -3,8 +3,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 from hls_webapp import db, bcrypt
 from hls_webapp.models import User, SoundFile
 from hls_webapp.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
-                                   RequestResetForm, ResetPasswordForm, SimulationOptions)
-from hls_webapp.users.utils import save_picture, send_reset_email , save_audio
+                                    RequestResetForm, ResetPasswordForm, SimulationOptions)
+from hls_webapp.users.utils import save_picture, send_reset_email, save_audio
 from hls_webapp.offline_wav_file import compute
 import os
 
@@ -17,8 +17,10 @@ def register():
         return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
@@ -58,9 +60,9 @@ def account():
             current_user.image_file = picture_file
 
         if form.audio.data:
-                audio_file = save_audio(form.audio.data)
-                audio_file = SoundFile(file_name=audio_file,user=current_user)
-                db.session.add(audio_file)
+            audio_file = save_audio(form.audio.data)
+            audio_file = SoundFile(file_name=audio_file, user=current_user)
+            db.session.add(audio_file)
 
         current_user.username = form.username.data
         current_user.email = form.email.data
@@ -70,30 +72,41 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
 
 @users.route("/user/output")
 @login_required
 def output():
-    compute("hls_webapp/ans_02.wav", "hls_webapp/output.wav")
-    return send_file("output.wav")
+    file_name = session[current_user.id][file_name]
+    # compute("hls_webapp/ans_02.wav", "hls_webapp/output.wav")
+    # return send_file("output.wav")
+    # audio_file_id = audio_file
+    return render_template('output.html', title='output', file_name=file_name)
+
 
 @users.route("/user/<string:username>/simulation", methods=['GET', 'POST'])
 @login_required
 def user_simulation(username):
-    path='audio_files/'
-    file_path = os.path.join('/static', path).replace('\\','/')
+    path = 'audio_files/'
+    file_path = os.path.join('/static', path).replace('\\', '/')
 
     form = SimulationOptions()
-    if request.method == 'GET':
-        sound_files = SoundFile.query.filter_by(user_id=current_user.id)
-        categories = [(g.id, g.file_name) for g in sound_files]
-        form.group_id.choices = categories
-    return render_template('simulation_setup.html', title='Simulation Set Up',
-                            form=form, file_path=file_path)
+    sound_files = SoundFile.query.filter_by(user_id=current_user.id)
+    choices_audio = [(g.id, g.file_name) for g in sound_files]
+    form.group_id.choices = choices_audio
 
+    if form.validate_on_submit():
+        audio_file_id = int(form.group_id.data)
+        file_name = form.group_id.choices[audio_file_id - 1][1]
+        flask.session[current_user.id] = file_name
+        return redirect(url_for('users.output'))
+        # return render_template('output.html', title='output', audio_file_id=audio_file_id, file_path=file_path)
+
+    return render_template('simulation_setup.html', title='Simulation Set Up', form=form, file_path=file_path)
 
 
 @users.route("/reset_password", methods=['GET', 'POST'])
@@ -119,7 +132,8 @@ def reset_token(token):
         return redirect(url_for('users.reset_request'))
     form = ResetPasswordForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
         user.password = hashed_password
         db.session.commit()
         flash('Your password has been updated! You are now able to log in', 'success')
